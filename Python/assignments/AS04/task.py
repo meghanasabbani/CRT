81cbd45ae46468fca929a7c2e206b6e13784a4eb
meghanasabbani <meghanasabbani007@gmail.com>
2026-03-07 12:56:43 +0530

    assignments

--- Python/assignments/AS04/task.py
+++
@@ -1,9 +0,0 @@
-def Reverse_String(s: str) -> str:
-    rev = ""
-    for i in range(len(s) - 1, -1, -1):
-        rev = rev+s[i]
-    return rev
-
-if __name__ == "__main__":
-    s = input()
-    print(Reverse_String(s))
