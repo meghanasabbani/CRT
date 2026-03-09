39f21fbab94a40842aff63753271e3700d0e4d81
meghanasabbani <meghanasabbani007@gmail.com>
2026-03-09 12:08:33 +0530

    M01 Task

--- Python/assignments/M01_Language_and_Programming_Basics/AST02/task.py
+++ Python/assignments/M01_Language_and_Programming_Basics/AST02/task.py
@@ -1,13 +1,13 @@
 def even_odd(n):
     if n%2==0:
         if n>=2 and n<=5:
-            return "Not weird"
-        if n>=6 and n<=20:
-            return "weird"
-        if n>20:
-            return "Not weird"
+            return "Not Weird"
+        elif n>=6 and n<=20:
+            return "Weird"
+        else:
+            return "Not Weird"
     else:
-        return "weird"
+        return "Weird"
 if __name__ == "__main__":
     n = int(input())
     print(even_odd(n))  
